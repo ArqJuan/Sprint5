704e14c5ddd59163dcc527bad5637045ec161914
Juan Hurtado <arq.juanhurtado.94@gmail.com>
2024-03-29 09:27:08 -0600

    Cambio por revision, de prueba

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ print(car_data.head(20))
 st.title('Creacion de Histrogramas')
 st.subheader('Lectura de archivo vehicle_us.csv', divider='blue')
 
+#Creacion de 2 Histogramas
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
@@ -26,12 +27,21 @@ if hist_button:
     fig = px.scatter(car_data, x = 'odometer', y = 'price')
     st.plotly_chart(fig, use_container_width=True)
 
+
+#Creacion de Histograma
 build_hist = st.checkbox('Construir un histograma')
 if build_hist: # al hacer clic en el botón
     
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+    st.write('Se esta comparando el kilometraje final del ')
+
+
+#creacion de checkbox para Grafica de Dispersion
+build_hist = st.checkbox('Construir una Grafica de dispersion')
+if build_hist: # al hacer clic en el botón
+    st.write('Histograma comparando precios de vehiculos vs su Kilometraje')
     
 if build_hist:
     st.write('Creacion de grafico de disperion')
